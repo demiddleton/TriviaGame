797f3aa44a1c222a446370d5cdb128d25e3e66d0
Denise Middleton <demiddleton@bellsouth.net>
2019-08-09 20:58:04 -0400

    pseudocode js

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+//Create a start button and hide trivia questions until the start button is clicked
+//Reveal trivia questions when the start button is clicked
+//Create timer to countdown from 15 seconds once the start button is clicked
+//Player cannot pick more than one answer per question
+//Display screen saying "Game Over" and number of correct answers and # of incorrect answers
