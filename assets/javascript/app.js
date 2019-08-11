4fb60cb0a71f12c9d25f3256aa090ef3cff91849
Denise Middleton <demiddleton@bellsouth.net>
2019-08-11 13:21:41 -0400

    declared variables for timer & created function to display stats when time is over

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,36 @@
+//Create variable to hold the setInterval timer
+var intervalID;
+
+//Create variable equal to the start time
+var time = 15;
+
+//Create variable to track the number of questions answered correctly
+var correct = 0;
+
+
 //Create a function to hide the start button when it is clicked
-$("#startButton").click(function() {
+$("#startButton").click(function () {
     $(".start").hide();
     $(".questions").show();
+    intervalID = setInterval(count, 1000);
+    function count(){
+        $("#timeRemaining-text").text("You have " + time + " seconds left." );
+        time--;
+
+        if(time < 0){
+            $(".questions").hide();
+            clearInterval(intervalID);
+            $("#timeRemaining-text").hide();
+            $("#done-text").show();
+            $("#done-text").text("You answered " + correct + " out of the three questions correctly." );
+        }
+
+    }
+
 });
 
-//Reveal trivia questions when the start button is clicked
-//Create timer to countdown from 15 seconds once the start button is clicked
+ //Create timer to countdown from 15 seconds once the start button is clicked
+
+
 //Player cannot pick more than one answer per question
 //Display screen saying "Game Over" and number of correct answers and # of incorrect answers
