8d79f9ffac2e233fa5c873abf2fb3d57b852ca70
Denise Middleton <demiddleton@bellsouth.net>
2019-08-11 12:05:53 -0400

    created a function to hide the start button and show questions when the start button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,8 @@
 //Create a function to hide the start button when it is clicked
+$("#startButton").click(function() {
+    $(".start").hide();
+    $(".questions").show();
+});
 
 //Reveal trivia questions when the start button is clicked
 //Create timer to countdown from 15 seconds once the start button is clicked
