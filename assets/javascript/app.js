87180901d1a31f544529b18739ea0365fb82522a
Denise Middleton <demiddleton@bellsouth.net>
2019-08-16 18:16:46 -0400

    created 3 functions to compare the answer checked to the correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,76 @@
-//Declare variable to hold the setInterval timer
-var intervalID;
+$(document).ready(function () {
 
-//Declare variable equal to the start time
-var time = 15;
+    //Declare variable to hold the setInterval timer
+    var intervalID;
 
-//Declare variable to track the number of questions answered correctly
-var correct = 0;
+    //Declare variable equal to the start time
+    var time = 10;
 
-//
+    //Declare variable to track the number of questions answered correctly
+    var correct = 0;
 
-//Create a function to hide the start button when it is clicked
-$("#startButton").click(function () {
-    $(".start").hide();
-    $(".questions").show();
+    //Create a function to hide the start button when it is clicked
+    $("#startButton").click(function () {
+        $(".start").hide();
+        $(".questions").show();
 
-    //Create timer to countdown from 15 seconds once the start button is clicked
-    intervalID = setInterval(count, 1000);
-    function count() {
-        $("#timeRemaining-text").text("You have " + time + " seconds left.");
-        time--;
+        //Create timer to countdown from 15 seconds once the start button is clicked
+        intervalID = setInterval(count, 1000);
+        function count() {
+            $("#timeRemaining-text").text("You have " + time + " seconds left.");
+            time--;
 
-        if (time < 0) {
-            $(".questions").hide();
-            clearInterval(intervalID);
-            $("#timeRemaining-text").hide();
-            $("#done-text").show();
-            $("#done-text").text("You answered " + correct + " out of the 3 questions correctly.");
-          
+            if (time < 0) {
+                $(".questions").hide();
+                clearInterval(intervalID);
+                $("#timeRemaining-text").hide();
+                $("#done-text").show();
+                $("#done-text").text("You answered " + correct + " out of the 3 questions correctly.");
+
+            }
         }
-    }
-    //Player cannot pick more than one answer per question
-    $(document).ready(function(){
-        $('.q1').click(function() {
+
+        //Player cannot pick more than one answer per question    
+        $('.q1').click(function () {
             $('.q1').not(this).prop('checked', false);
+            console.log(this);
         });
+        correct = q1Function(correct);
 
-        $('.q2').click(function() {
+        $('.q2').click(function () {
             $('.q2').not(this).prop('checked', false);
+            console.log(this);
         });
+        correct = q2Function(correct);
 
-        $('.q3').click(function() {
+
+        $('.q3').click(function () {
             $('.q3').not(this).prop('checked', false);
+            console.log(this);
         });
-    });
 
-    //Create function to increase number of correct answers
-    $("#q1Checkbox4").click(function () {
-        correct++;
-    });
-
-    $("#q2Checkbox2").click(function () {
-        correct++;
-    });
-
-    $("#q3Checkbox2").click(function () {
-        correct++;
+        correct = q3Function(correct);
     });
 
 });
 
+function q1Function(correct) {
+    if ($("#q1Checkbox4")) {
+        correct++;
+    }
+    return correct;
+}
 
+function q2Function(correct) {
+    if ($("#q2Checkbox2")) {
+        correct++;
+    }
+    return correct;
+}
 
-
-
+function q3Function(correct) {
+    if ($("#q3Checkbox2")) {
+        correct++;
+    }
+    return correct;
+}
