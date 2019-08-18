20994be111ed66a0b89530fa47f82eb6e4b23698
Denise Middleton <demiddleton@bellsouth.net>
2019-08-18 12:32:28 -0400

    created text to display correct and incorrect answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,15 +6,17 @@ $(document).ready(function () {
     //Declare variable equal to the start time
     var time = 10;
 
-    //Declare variable to track the number of questions answered correctly
+    //Declare variables to track the number of questions answered correctly & incorrectly
     var correct = 0;
+    var incorrect = 0;
 
     //Create a function to hide the start button when it is clicked
     $("#startButton").click(function () {
         $(".start").hide();
         $(".questions").show();
-    });
-    //Create timer to countdown from 15 seconds once the start button is clicked
+   
+
+        //Create timer to countdown from 15 seconds once the start button is clicked
     intervalID = setInterval(count, 1000);
     function count() {
         $("#timeRemaining-text").text("You have " + time + " seconds left.");
@@ -29,25 +31,38 @@ $(document).ready(function () {
             console.log(q1Value);
             if (q1Value === "option4") {
                 correct++;
+            } else {
+                incorrect++;
             }
 
             var q2Value = $('.q2:checked').val();
             console.log(q2Value);
             if (q2Value === "option2") {
                 correct++;
+            } else {
+                incorrect++;
             }
 
             var q3Value = $('.q3:checked').val();
             console.log(q3Value);
             if (q3Value === "option2") {
                 correct++;
+            } else {
+                incorrect++;
             }
 
             $("#done-text").show();
-            $("#done-text").text("You answered " + correct + " out of the 3 questions correctly.");
+            $("#done-text").text("All Done!");
+
+            $("#correct-text").show();
+            $("#correct-text").text("Correct Answers: " + correct);
+
+            $("#incorrect-text").show();
+            $("#incorrect-text").text("Incorrect Answers: " + incorrect);
         }
     }
 
+});
 
 });
 
