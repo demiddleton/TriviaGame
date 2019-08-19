001ffdf682853e8cc9e34f8ebbc5f6b2e6f67925
Denise Middleton <demiddleton@bellsouth.net>
2019-08-18 21:35:53 -0400

    changed timer from 10 secs to 30 secs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,65 +4,89 @@ $(document).ready(function () {
     var intervalID;
 
     //Declare variable equal to the start time
-    var time = 10;
+    var time = 30;
 
-    //Declare variables to track the number of questions answered correctly & incorrectly
+    //Declare variables to track the number of questions answered correctly, incorrectly & unanswered
     var correct = 0;
     var incorrect = 0;
+    var unanswered = 0;
 
     //Create a function to hide the start button when it is clicked
     $("#startButton").click(function () {
         $(".start").hide();
         $(".questions").show();
-   
+
 
         //Create timer to countdown from 15 seconds once the start button is clicked
-    intervalID = setInterval(count, 1000);
-    function count() {
-        $("#timeRemaining-text").text("You have " + time + " seconds left.");
-        time--;
+        intervalID = setInterval(count, 1000);
+        function count() {
+            $("#timeRemaining-text").text("You have " + time + " seconds left.");
+            time--;
 
-        if (time < 0) {
-            $(".questions").hide();
-            clearInterval(intervalID);
-            $("#timeRemaining-text").hide();
+            if (time < 0) {
+                $(".questions").hide();
+                clearInterval(intervalID);
+                $("#timeRemaining-text").hide();
 
-            var q1Value = $('.q1:checked').val();
-            console.log(q1Value);
-            if (q1Value === "option4") {
-                correct++;
-            } else {
-                incorrect++;
+                q1IsChecked();
+                q2IsChecked();
+                q3IsChecked();
+
+
+                $("#done-text").show();
+                $("#done-text").text("All Done!");
+
+                $("#correct-text").show();
+                $("#correct-text").text("Correct Answers: " + correct);
+
+                $("#incorrect-text").show();
+                $("#incorrect-text").text("Incorrect Answers: " + incorrect);
+
+                $("#unanswered-text").show();
+                $("#unanswered-text").text("Unanswered: " + unanswered);
             }
+        }
 
-            var q2Value = $('.q2:checked').val();
-            console.log(q2Value);
-            if (q2Value === "option2") {
-                correct++;
-            } else {
-                incorrect++;
-            }
-
-            var q3Value = $('.q3:checked').val();
-            console.log(q3Value);
-            if (q3Value === "option2") {
-                correct++;
-            } else {
-                incorrect++;
-            }
-
-            $("#done-text").show();
-            $("#done-text").text("All Done!");
-
-            $("#correct-text").show();
-            $("#correct-text").text("Correct Answers: " + correct);
-
-            $("#incorrect-text").show();
-            $("#incorrect-text").text("Incorrect Answers: " + incorrect);
+    });
+    function q1IsChecked() {
+        var q1Value = $('.q1:checked').val();
+        console.log(q1Value);
+        if ($(".q1").val() == "_blank") {
+            unanswered++;
+        }
+        
+        if (q1Value != "option4") {
+            incorrect++;
+        } else {
+            correct++;
         }
     }
 
-});
-
-});
+        function q2IsChecked() {
+            var q2Value = $('.q2:checked').val();
+            console.log(q2Value);
+            if ($(".q2").val() == "_blank") {
+                unanswered++;
+            }
+            
+            if (q2Value != "option4") {
+                incorrect++;
+            } else {
+                correct++;
+            }
+        }
+        function q3IsChecked() {
+            var q3Value = $('.q3:checked').val();
+            console.log(q3Value);
+            if ($(".q3").val() == "_blank") {
+                unanswered++;
+            } 
+            
+            if (q3Value != "option4") {
+                incorrect++;
+            } else {
+                correct++;
+            }
+        }
+    });
 
